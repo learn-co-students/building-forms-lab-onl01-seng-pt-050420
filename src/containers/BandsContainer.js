5266e46f3f50c6032ea1b5de407bdd4f3234b263
James Rogers <jarogers095@gmail.com>
2021-03-07 19:28:59 -0800

    Done.

--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import BandInput from '../components/BandInput';
 
 class BandsContainer extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  addBand = (name) => {
+    this.props.dispatch({ type: "ADD_BAND", name: name });
+  }
+
   render() {
     return(
       <div>
-        BandsContainer
+        <BandInput addBand={this.addBand}/>
+        <br />
+        <ul>
+          {this.props.bands.map((band) => <li>{band.name}</li>)}
+        </ul>
       </div>
     )
   }
 }
 
-export default BandsContainer
+const mapStateToProps = (state) => ({bands: state.bands});
+
+export default connect(mapStateToProps)(BandsContainer);
+ 
