5c9385f8648bb37459c72c90c6fb119cfd2ecb75
Awilliams4321 <awilliams1234@live.com>
2021-03-26 10:20:10 -0600

    Done.

---
+++ src/components/Band.js
@@ -0,0 +1,11 @@
+
+import React, { Component } from 'react'
+
+export default class Band extends Component { 
+    render() {
+        
+        return (
+            <li>{this.props.band.name}</li>
+        )
+    }
+} 
